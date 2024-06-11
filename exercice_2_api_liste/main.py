19118953202147d98d961fb5123947fd8f888d69
DaiTensa <dai.tensaout@gmail.com>
2024-06-11 16:00:14 +0200

    version 1 api courses

--- exercice_2_api_liste/main.py
+++ exercice_2_api_liste/main.py
@@ -2,4 +2,40 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-ma_liste_integer = []
+liste_courses = {}
+
+@app.get("/")
+def index():
+    return {"message": "bonjour bienvenu sur l’API liste de course"}
+
+
+@app.get('/liste')
+def get_list():
+    if not liste_courses:
+        return {"message": "La liste est vide"}
+    else:
+        return {"content" : liste_courses}
+    
+
+@app.post('/liste')
+def add_to_list(element:str, quantity:int, unit:str = ""):
+    quantity_unit = {}
+    if element in liste_courses:
+        liste_courses[element]["Q"] += quantity
+    else:
+        quantity_unit["Q"] = quantity
+        quantity_unit["U"] = unit
+        liste_courses[element] = quantity_unit
+    return liste_courses
+
+
+@app.delete('/liste')
+def remove_from_liste(element:str):
+    del liste_courses[element]
+    return liste_courses
+
+
+@app.delete('/liste/clear')
+def clear_liste():
+    liste_courses.clear()
+    return liste_courses

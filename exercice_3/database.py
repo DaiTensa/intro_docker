c5686d8de510700692eb017c0d1064f67e59bd9f
DaiTensa <dai.tensaout@gmail.com>
2024-06-13 14:26:27 +0200

    last commit

--- exercice_3/database.py
+++ exercice_3/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./courses_liste.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
